9f1f341fd2536ef6352696fc0bb36550c82d62d8
Stephanie Brink <brink2@llnl.gov>
2025-09-22 14:13:04 -0700

    remove hidden checkpoint files, add to .gitignore

--- treescape/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,5 +0,0 @@
-from .StackedLine import StackedLine
-from .TreeScapeModel import TreeScapeModel
-from .CaliReader import CaliReader
-from .MultiLine import MultiLine
-from .StackedLinePython import StackedLinePython
